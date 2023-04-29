abf6dd07fa8c89e97247f46b214ad8e8a6c3b765
victorhalimm <victorhalim18@gmail.com>
2023-04-29 21:10:44 +0700

    fix

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,6 +1,6 @@
 const LoadingPage = () => {
     return (
-        <div className="bg-black w-screen h-screen flex justify-center items-center">
+        <div className="bg-black w-screen h-screen flex justify-center items-center z-50 relative">
             <div class="loader">
                 <div class="justify-content-center jimu-primary-loading"></div>
             </div>
